d21c06185df88c339db4f5d031943c1a56fc87fd
Adeola Oladeji <deolaoladeji@gmail.com>
2023-07-03 21:41:04 +0100

    Patient Controller

--- src/main/java/com/deezproject/electronic/health/record/controller/PatientController.java
+++ src/main/java/com/deezproject/electronic/health/record/controller/PatientController.java
@@ -1,7 +1,11 @@
 package com.deezproject.electronic.health.record.controller;
 
 import com.deezproject.electronic.health.record.data.dto.request.PatientRegisterDto;
+import com.deezproject.electronic.health.record.data.dto.response.ApiResponse;
+import com.deezproject.electronic.health.record.data.exception.DuplicatePatientException;
+import com.deezproject.electronic.health.record.data.exception.InvalidEmailException;
 import com.deezproject.electronic.health.record.service.PatientService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +20,15 @@ public class PatientController {
 
     @PostMapping("/register")
     public ResponseEntity<?> registerPatient(@RequestBody PatientRegisterDto request){
-//        return patientService.registerPatient(request);
-        return null;
+        try{
+            var response =  patientService.registerPatient(request);
+            return ResponseEntity.ok(response);
+        }catch (InvalidEmailException | DuplicatePatientException exception){
+            ApiResponse<?> response = ApiResponse.builder()
+                    .message(exception.getMessage())
+                    .statusCode(HttpStatus.BAD_REQUEST.value())
+                    .build();
+            return ResponseEntity.badRequest().body(request);
+        }
     }
 }
--- src/main/java/com/deezproject/electronic/health/record/data/dto/response/ApiResponse.java
+++ src/main/java/com/deezproject/electronic/health/record/data/dto/response/ApiResponse.java
@@ -1,9 +1,15 @@
 package com.deezproject.electronic.health.record.data.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
     private String message;
